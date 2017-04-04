de74b8c2d24724aac179378ce651402ad1fc4c97
Kentaro Haneda <hub>
2017-04-04 14:46:10 +0900

    Fraction

---
+++ 2017/gasshuku/class/Fraction_main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <vector>
+#include "Fraction.hpp"
+
+using namespace std;
+
+int main() {
+  vector<Fraction> v;
+  int a, b;
+
+  while (cin >> a >> b, a != 0) {
+    v.push_back(Fraction(a, b));
+  }
+
+  for ()
+}
